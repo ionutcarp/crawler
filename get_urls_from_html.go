f6623e6b4762589d169b1d5ea5ac3f2775472e1e
ionutcarp <carpionut@gmail.com>
2025-02-07 16:44:35 +0200

    Web Crawler L5

---
+++ get_urls_from_html.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
+	doc, err := html.Parse(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse HTML: %v", err)
+	}
+	var urls []string
+	for n := range doc.Descendants() {
+		if n.Type == html.ElementNode && n.DataAtom == atom.A {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					href, err := url.Parse(a.Val)
+					if err != nil {
+						fmt.Printf("couldn't parse href '%v': %v\n", a.Val, err)
+						continue
+					}
+					urls = append(urls, baseURL.ResolveReference(href).String())
+					break
+				}
+			}
+		}
+	}
+
+	return urls, nil
+}
